8d4ffa48f1b5e473fcb9e40a192ea79c7cb7b3cd
Kenneth van Ewijk <kennyboy@live.nl>
2015-02-24 13:37:05 +0100

    Create PanelDate.java

---
+++ PanelDate.java
@@ -0,0 +1,13 @@
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+
+public class PanelDate extends JPanel {
+	
+	public PanelDate()
+	{
+		super();
+		setBackground(new Color(255,155,155));
+	}
+}
